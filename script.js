7987ea87beba2e33ed9ba8dea82796d40791f186
juny62017 <juny62017@gmail.com>
2026-05-19 01:22:35 +0530

    Added player spaceship and movement controls

--- script.js
+++ script.js
@@ -4,4 +4,127 @@ let canvas =
 let ctx =
     canvas.getContext("2d");
 
-console.log("GalaxyBlitz Started");
+let player = {
+
+    x: 180,
+
+    y: 560,
+
+    width: 60,
+
+    height: 60,
+
+    speed: 6
+
+};
+
+let keys = {};
+
+document.addEventListener(
+    "keydown",
+    function (event) {
+
+        keys[event.key] = true;
+
+    }
+);
+
+document.addEventListener(
+    "keyup",
+    function (event) {
+
+        keys[event.key] = false;
+
+    }
+);
+
+function drawPlayer() {
+
+    ctx.fillStyle =
+        "#5bd1ff";
+
+    ctx.beginPath();
+
+    ctx.moveTo(
+        player.x + player.width / 2,
+        player.y
+    );
+
+    ctx.lineTo(
+        player.x,
+        player.y + player.height
+    );
+
+    ctx.lineTo(
+        player.x + player.width,
+        player.y + player.height
+    );
+
+    ctx.closePath();
+
+    ctx.fill();
+
+}
+
+function movePlayer() {
+
+    if (
+        keys["ArrowLeft"] &&
+        player.x > 0
+    ) {
+
+        player.x -= player.speed;
+
+    }
+
+    if (
+        keys["ArrowRight"] &&
+        player.x + player.width <
+        canvas.width
+    ) {
+
+        player.x += player.speed;
+
+    }
+
+    if (
+        keys["ArrowUp"] &&
+        player.y > 0
+    ) {
+
+        player.y -= player.speed;
+
+    }
+
+    if (
+        keys["ArrowDown"] &&
+        player.y + player.height <
+        canvas.height
+    ) {
+
+        player.y += player.speed;
+
+    }
+
+}
+
+function gameLoop() {
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    movePlayer();
+
+    drawPlayer();
+
+    requestAnimationFrame(
+        gameLoop
+    );
+
+}
+
+gameLoop();
